40842d7143ecd26a4dce46ba1248823a63bc925c
vcntsnhz <vicente.sanhueza1901@alumnos.ubiobio.cl>
2022-11-14 20:17:19 -0300

    correcion de errores

--- routes/cuidadorRoutes.js
+++ routes/cuidadorRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const api = express.Router();
 const cuidadorController = require('../controllers/cuidadorController');
 
-api.post('/cuidador/:id', cuidadorController.createCuidador);
+api.post('/cuidador', cuidadorController.createCuidador);
 api.get('/cuidadores', cuidadorController.getCuidadors);
 api.get('/cuidador/search/:id', cuidadorController.getCuidador);
 api.put('/cuidador/update/:id', cuidadorController.updateCuidador);
