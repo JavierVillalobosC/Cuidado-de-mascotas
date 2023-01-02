a7bd864ab0a00c30af27d90b0c0b52617270f23d
Sebasaraneda <sebastian.araneda1901@alumnos.ubiobio.cl>
2023-01-02 06:19:38 -0300

    Vecinos fronted

---
+++ fronted/pages/vecino/[vecino].jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import { Container, Heading, Text, Button, Stack, Input, FormControl, FormLabel, Textarea } from '@chakra-ui/react'
+import { getVecinos } from '../../../data/vecinos'
+import { getVecino } from '../../../backend/controllers/vecinoController'
+
+export async function getServerSideProps(context) {
+    try {
+        const response = await getVecino(context.query.vecino, context.req.headers.cookie)
+        return {
+            props: {
+                data: response.data
+            }
+        }
+    } catch (err) {
+        return {
+            redirect: {
+                destination: '/vecinos',
+                permanent: true
+            }
+        }
+    }
+}
+
+
+const vecino = ({ data }) => {
+
+    const router = useRouter()
+    const { vecino } = router.query
+    const [vecin] = useState(data)
+
+
+    return (
+        <Container maxW="container.md">
+            <Heading >{vecino.name}</Heading>
+            <Heading> Editar</Heading>
+        </Container>
+
+    )
+}
+
+export default vecino
---
+++ fronted/pages/vecinos.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react'
+import { Button, Container, Input, Stack, Text, HStack, Table, Thead, Tbody, Tfoot, Tr, Th, Td, Heading, } from '@chakra-ui/react'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+
+const vecinos = () => {
+    const [vecinos, setVecinos] = useState([])
+    const router = useRouter()
+
+    const getVecinos = async () => {
+        const response = await axios.get(`${process.env.API_URL}/vecinos`)
+        setVecinos(response.data)
+    }
+
+    useEffect(() => {
+        getVecinos()
+    }, [])
+
+
+    const showVecinos = () => {
+        return vecinos.map(vecino => {
+            return (
+                <Tr key={vecino._id}>
+                    <Td>{vecino.rut}</Td>
+                    <Td>{vecino.name}</Td>
+                    <Td>{vecino.lastname}</Td>
+                    <Td>{vecino.address}</Td>
+                    <Td>{vecino.email}</Td>
+                    <Td>{vecino.phone}</Td>
+                    <Td><Button onClick={() => router.push(`/vecino/ver/${vecino._id}`)}>Ver mas</Button></Td>
+                </Tr>
+            )
+        })
+    }
+
+    return (
+        <Container maxW="container.xl" centerContent>
+            <Heading textAlign={"center"} my={10}>Vecinos</Heading>
+            <Button colorScheme="teal" onClick={() => router.push('/vecino/crear')}>Crear Vecino</Button>
+            <Table variant="simple">
+                <Thead>
+                    <Tr>
+                        <Td>Nombre</Td>
+                        <Td>Rut</Td>
+                        <Td>Mascota</Td>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {showVecinos()}
+                </Tbody>
+            </Table>
+        </Container>
+    )
+}
+
+export default vecinos
