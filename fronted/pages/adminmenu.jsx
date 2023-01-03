77dcf00b5701c60ca6ded18a2a6f17b1e45c0463
vcntsnhz <vicente.sanhueza1901@alumnos.ubiobio.cl>
2023-01-03 05:33:21 -0300

    UPDATE v3

--- fronted/pages/adminmenu.jsx
+++ fronted/pages/adminmenu.jsx
@@ -32,6 +32,16 @@ export default function adminmenu() {
 		router.push('/vermascotas')
 	}
 
+	const onvecinos = async (e) => {
+		e.preventDefault()
+		router.push('/vecinos')
+	}
+
+	const oncuidadores = async (e) => {
+		e.preventDefault()
+		router.push('/cuidadores')
+	}
+
   return (
 
     <Container maxW="container.xl" centerContent>
@@ -40,6 +50,9 @@ export default function adminmenu() {
 			<Button colorScheme="teal" onClick={onretiros} size='lg'> Ver Retiros</Button>
             <Button colorScheme="teal" onClick={oningresos} size='lg'> Ver Ingresos</Button>
 			<Button colorScheme="teal" onClick={onvermascotas} size='lg'> Ver Mascotas</Button>
+			<Button colorScheme="teal" onClick={onvecinos} size='lg'> Ver Vecinos</Button>
+			<Button colorScheme="teal" onClick={oncuidadores} size='lg'> Ver Cuidadores</Button>
+
 			</Stack>
             <Stack direction ='row' spacing={4} py='4'>
 			<Button colorScheme="teal" onClick={oncrearmascotas} size='lg'> Ingresar Mascotas</Button>
