35022f75a368307e4bfff0a3742d8d4419746cf1
Javier Villalobos <javi.nicovill@gmail.com>
2023-01-02 21:26:57 -0300

    a

--- fronted/pages/index.jsx
+++ fronted/pages/index.jsx
@@ -1,6 +1,6 @@
 
 import {useState} from 'react'
-import {Button, Container, Input, Heading, Stack} from '@chakra-ui/react'
+import {Button, Container, Input, Heading, Stack,Box, Flex} from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 
 export default function Home() {
@@ -10,23 +10,25 @@ export default function Home() {
 
   const oncuidador = async (e) => {
 		e.preventDefault()
-		router.push('/ingreso')
+		router.push('/adminmenu')
 	}
 
   const onvecino = async (e) => {
 		e.preventDefault()
-    router.push('/ingresos')
+    router.push('/vecinomenu')
 	}
 
   return (
-
+    
     <Container maxW="container.xl" centerContent>
-			<Heading textAlign={"center"} my={100}>Seleccione el perfil</Heading>
+			<Heading textAlign={"center"} my={100}>Seleccione al usuario</Heading>
 			<Stack direction ='row' spacing={4}>
-				<Button colorScheme="teal" onClick={oncuidador} size='lg'>Cuidador</Button>
-        <Button colorScheme="teal" onClick={onvecino} size='lg'>vecino</Button>
+			<Button colorScheme="teal" onClick={oncuidador} size='lg'> Cuidador</Button>
+            <Button colorScheme="teal" onClick={onvecino} size='lg'> Vecino</Button>
 			</Stack>
+            
 		</Container>
+	
 
   )
 }
